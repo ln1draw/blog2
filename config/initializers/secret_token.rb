7fb39b89a670e0f96ed44594428f12d80e7ce0ec
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-01-10 12:40:35 -0800

    omfg user authentication is working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '16e2e6ed4d6598c93ecefaca345bc5870f5fe2af2e68b8a6cb9f49303ac2ad6a782fe4830c886fbddf4e40d460ba6ba05bceef11c6ce3de116a8f55deb581dde'
---
+++ db/migrate/20140110202947_add_password_etc.rb
@@ -0,0 +1,5 @@
+class AddPasswordEtc < ActiveRecord::Migration
+  def change
+    add_column :users, :password, :string
+  end
+end
