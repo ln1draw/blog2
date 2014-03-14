d1a54768c9be65ddb78a9417d687032cac1c228a
shanghaiellen <shanghaiellen@gmail.com>
2014-03-13 20:53:19 -0700

    accordion is more or less put together

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,43 +1,6 @@
 $(document).ready(function (){
-  var descriptions = ['#scamazon-box', '#scamalytics-box', '#feedhunter-box', '#llama-lister-box', '#pea-patch-box','#sloth-expresser-box'
-  ];
-
-  var DisplayDescription = function(new_description, active_tab){
-    $(descriptions).each(function(index, description){
-      $(description).hide();
-    });
-    $(new_description).show();
-    $(active_tab).addClass('active');
-    $(new_description).addClass('active_description');
-    $('.active').click(function(){
-      $('.active').removeClass('active');
-      $('.active_description').hide();
-      $('.active_description').removeClass('active_description');
-    });
-  };
-
-  $('#scamazon').click(function(){
-    DisplayDescription('#scamazon-box', '#scamazon');
-  });
-
-  $('#scamalytics').click(function(){
-    DisplayDescription('#scamalytics-box', '#scamalytics');
-  });
-
-  $('#feedhunter').click(function(){
-    DisplayDescription('#feedhunter-box', '#feedhunter');
-  });
-
-  $('#llama-lister').click(function(){
-    DisplayDescription('#llama-lister-box', '#llama-lister');
-  });
-
-  $('#pea-patch').click(function(){
-    DisplayDescription('#pea-patch-box', '#pea-patch');
-  });
-
-  $('#sloth-expresser').click(function(){
-    DisplayDescription('#sloth-expresser-box', '#sloth-expresser');
-  });
-
-})
+  var collapsible = $( ".ui.accordion" ).accordion({collapsible: true, animated: false});
+ 
+  // setter
+  $( ".ui.accordion" ).accordion( "option", "collapsible", true );
+});
