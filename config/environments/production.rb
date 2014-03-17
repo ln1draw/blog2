95a4350812a284cbcfe67f7b91041fb7c5efee29
shanghaiellen <shanghaiellen@gmail.com>
2014-03-17 12:54:24 -0700

    adds compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Blog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
