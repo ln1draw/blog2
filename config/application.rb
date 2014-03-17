ff4cc932b1c1cf21d4fb44934d4782c88a741b78
shanghaiellen <shanghaiellen@gmail.com>
2014-03-17 13:44:57 -0700

    please dont fail me now

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Blog
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -20,5 +19,6 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.serve_static_assets = true
   end
 end
